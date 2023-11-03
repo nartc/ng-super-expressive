a3ae3f9fb6075e9e4f263221f512300ed633c913
Chau Tran <nartc7789@gmail.com>
2023-11-03 14:09:26 -0500

    add copy

--- src/app/playground/result/result.ts
+++ src/app/playground/result/result.ts
@@ -1,3 +1,4 @@
+import { ClipboardModule } from '@angular/cdk/clipboard';
 import { Component, signal } from '@angular/core';
 import { injectOutput } from '../output';
 import { Highlight } from './highlight';
@@ -7,10 +8,20 @@ import { Highlight } from './highlight';
 	standalone: true,
 	templateUrl: './result.html',
 	host: { class: 'contents' },
-	imports: [Highlight],
+	imports: [Highlight, ClipboardModule],
 })
 export class Result {
 	protected output = injectOutput();
 	protected input = signal('');
+
 	protected scrollTop = signal(0);
+	protected copyText = signal('Copy');
+
+	protected onCopied(didCopy: boolean) {
+		if (!didCopy) return;
+		this.copyText.set('Copied!');
+		setTimeout(() => {
+			this.copyText.set('Copy');
+		}, 1000);
+	}
 }
